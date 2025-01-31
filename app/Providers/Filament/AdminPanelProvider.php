9191c84362754561bbc63ce327cb9c56ab866f62
Ardhan2121 <novealdioardhan@gmail.com>
2025-02-01 01:50:57 +0700

    add custom font 'Outfit' to Filament admin panel configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->font('Outfit')
             ->sidebarCollapsibleOnDesktop()
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
